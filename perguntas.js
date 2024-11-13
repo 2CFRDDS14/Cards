9f4674d07566dfb2cb0c7cab310a7e2da242a28c
Felipe Ribeiro <00001094704519sp@al.educacao.sp.gov.br>
2024-11-13 08:15:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Billie Eilish',
+    'Qual a idade dela?',
+    '22 anos.'
+)
+
+criaCartao(
+   'Billie Eilish',
+'Quando ela nasceu?',
+   '18 de dezembro de 2001'
+)
+
+criaCartao(
+  'Billie Eilish',
+  'Quantos filhos a Billie Eilish tem?',
+  'Billie Eilish diz que “prefere morrer” a não ter filhos.'
+)
+
+criaCartao(
+    'Billie Eilish',
+    'Onde mora a cantora Billie?',
+    'Highland Park'
+)
+
+criaCartao(
+    'Billie Eilish',
+    'Qual a cor favorita da Billie Eilish?',
+    'Azul.'
+)
+
+criaCartao(
+    'Billie Eilish',
+    'Quantos Oscars a Billie Eilish tem?',
+    'Eilish se tornou a pessoa mais jovem a ganhar dois Oscars.'
+)
